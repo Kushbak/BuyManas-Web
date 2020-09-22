4c9e433d57c68111dd19d35218e47a08348e2c17
Kushbak Mamytov <mamytovkushbak@gmail.com>
2020-09-22 19:14:53 +0600

    added responsive for slider

--- src/components/Main/TopPosts/TopPosts.jsx
+++ src/components/Main/TopPosts/TopPosts.jsx
@@ -11,12 +11,35 @@ import Preloader from '../../common/Preloader/Preloader';
 const TopPosts = (props) => {
 
     const { t } = useTranslation();
-    let settings = {
+    const settings = {
         dots: false,
         infinite: false,
         speed: 500,
         slidesToShow: 5,
-        slidesToScroll: 1
+        slidesToScroll: 1, 
+        responsive: [
+            {
+                breakpoint: 1025,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 2, 
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            }
+        ]
     };
 
     const topPostsArray = [],
